d6a155b3efad5db7e664cd1c72682e1049ec23b3
blackhono <diegolar25@outlook.com>
2022-03-01 03:37:31 -0300

    Sequelize configurado, primeiro model, dica de como adicionar tabela com sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,7 +1,17 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res){
-    res.json({
-      tudoCerto: true
-    })
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Diego',
+      sobrenome: 'Lepamar',
+      email: 'diegolar25@outlook.com',
+      idade: 30,
+      peso: 76,
+      altura: 1.72,
+    });
+    res.json(novoAluno);
   }
 }
+
+export default new HomeController();
