df0bab4625c75e3102a6d2434e7ac66743e99ebd
blackhono <diegolar25@outlook.com>
2022-03-01 22:12:54 -0300

    CRUD usuarios Sequelize e senha com HASH

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import bcryptjs from 'bcryptjs';
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 12],
+            msg: 'Nome deve ter entre 3 e 12 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe!',
+        },
+        validate: {
+          isEmail: { msg: 'Email invalido' },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 12],
+            msg: 'A senha deve ter entre 6 e 12 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+}
